1d36b1f89bde56674691b09c259f633ed74f1fb1
Aldi Megantara <aldi.ma@logisklik.com>
2021-07-14 23:19:32 +0700

    adding key on outer element articlelist

--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -19,7 +19,7 @@ const ArticleList = ({ articles }) => {
       <Header />
         <div className={articleStyles.grid}>
           {articles.map((article) => (
-            <ArticleItem article={article} />
+            <ArticleItem article={article} key={article.id} />
           ))}
         </div>
       </PDFExport>
